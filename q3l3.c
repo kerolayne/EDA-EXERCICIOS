36be5d76c18cbae62f6440c46dc349895ec98f28
kerolayne <kerolaynesouza@outlook.com.br>
2019-04-02 20:32:57 -0300

    Create q3l3.c

---
+++ q3l3.c
@@ -0,0 +1,15 @@
+#include "pilha.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <.h>
+
+int main(int argc, char const *argv[]) {
+  int i; 
+  char s[30];  
+  
+
+  for(i = 0; s[i]!= '\0'; i++){
+    
+  }
+  return 0;
+}
